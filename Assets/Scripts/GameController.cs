7242e0570bebdf72fd733ad747178d62b48af4db
wickedManatee <wicked.manatee.games@gmail.com>
2020-06-19 16:38:26 -0500

    Added in AcornSpawner, Acorns, project folders

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float gameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+    }
+}
