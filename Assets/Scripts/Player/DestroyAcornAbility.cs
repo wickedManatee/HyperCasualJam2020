e53042cdb07f03ee679aec4b562bf175c1a74967
saporter <scott.porter.ii@gmail.com>
2020-06-19 22:18:33 -0500

    Destroy ability refactor

---
+++ Assets/Scripts/Player/DestroyAcornAbility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAcornAbility : Ability
+{
+    public override void ProcessAbility()
+    {
+        base.ProcessAbility();
+
+        if (Input.GetMouseButtonDown(0)) //mouse or phone touch
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            { //If we clicked on an acorn
+                if (hit.transform.name.StartsWith("Acorn"))
+                {
+                    hit.transform.GetComponent<AcornController>().DestroyAcorn();
+                }
+            }
+        }
+    }
+}
