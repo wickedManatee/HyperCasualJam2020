c907a37c99fdcab237e287c2228d8b2870ff47ba
saporter <scott.porter.ii@gmail.com>
2020-06-20 12:27:00 -0500

    branch controller init.  Bugs

--- Assets/Scripts/BranchController.cs
+++ Assets/Scripts/BranchController.cs
@@ -8,21 +8,39 @@ public class BranchController : MonoBehaviour
     public Collider BranchCollider;
     public Collider PlayerTrigger;
 
-    protected PlayerController _droplet;
+    protected PlayerController _player;
+    protected float _y;
 
     protected virtual void Awake()
     {
-        _droplet = FindObjectOfType<PlayerController>();
-        Assert.IsNotNull(_droplet);
+        _player = FindObjectOfType<PlayerController>();
+        Assert.IsNotNull(_player);
+        Assert.IsNull(_player.transform.parent, "Logic in this BranchController depends on the Player not being parented under any scene object.  Change Update() in this controller to use _player.transform.position, or unparent the Player from whatever it was just parented under.");
     }
 
     protected virtual void Start()
     {
-        PlayerTrigger.transform.position = transform.position + BranchCollider.bounds.extents + _droplet.PlayerCollider.bounds.size + PlayerTrigger.bounds.extents;
+        PlayerTrigger.transform.position = new Vector3( 
+            PlayerTrigger.transform.position.x,
+            transform.position.y + BranchCollider.bounds.extents.y + _player.PlayerCollider.bounds.size.y + PlayerTrigger.bounds.extents.y,
+            PlayerTrigger.transform.position.z
+            );
+        _y = transform.position.y;
     }
 
     protected virtual void Update()
     {
+        if(BranchCollider.enabled && _player.transform.localPosition.y < _y)
+        {
+            BranchCollider.enabled = false;
+        }
+    }
 
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if(BranchCollider.enabled && other.GetComponent<PlayerController>() != null)
+        {
+            BranchCollider.enabled = true;
+        }
     }
 }
