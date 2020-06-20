e6f5b68d21619f3c8435ed8df1c445294176ce59
saporter <scott.porter.ii@gmail.com>
2020-06-20 12:00:20 -0500

    working

---
+++ Assets/Scripts/BranchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class BranchController : MonoBehaviour
+{
+    public Collider BranchCollider;
+    public Collider PlayerTrigger;
+
+    protected PlayerController _droplet;
+
+    protected virtual void Awake()
+    {
+        _droplet = FindObjectOfType<PlayerController>();
+        Assert.IsNotNull(_droplet);
+    }
+
+    protected virtual void Start()
+    {
+        PlayerTrigger.transform.position = transform.position + BranchCollider.bounds.extents + _droplet.PlayerCollider.bounds.size + PlayerTrigger.bounds.extents;
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+}
