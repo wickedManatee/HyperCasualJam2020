12818e8c77122be461c2d8b91f9294f5aef3d678
saporter <scott.porter.ii@gmail.com>
2020-06-21 09:06:51 -0500

    rain and thunder

---
+++ Assets/Scripts/FX/LightningFlash.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFlash : MonoBehaviour
+{
+    public float FadeAwayTime = 0.5f;
+    protected CanvasGroup _cg;
+    protected Coroutine _flashing;
+
+    protected virtual void Awake()
+    {
+        _cg = FlashFinder.FlashCanvasGroup;
+    }
+    protected virtual void Start()
+    {
+        Flash();
+    }
+
+    public virtual void Flash()
+    {
+        _cg.alpha = 1f;
+        if (_flashing != null)
+            StopCoroutine(_flashing);
+        _flashing = StartCoroutine(FadeAway());
+    }
+
+    protected IEnumerator FadeAway()
+    {
+        var stopTime = Time.time + FadeAwayTime;
+        while(Time.time < stopTime)
+        {
+            yield return null;
+            _cg.alpha = (stopTime - Time.time) / stopTime;
+        }
+        _cg.alpha = 0f;
+    }
+}
