f08fa91df92b4af5c2ba0455d368a050c616bb16
saporter <scott.porter.ii@gmail.com>
2020-06-21 11:38:00 -0500

    Flowers init

---
+++ Assets/Scripts/FlowerRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerRandomizer : MonoBehaviour
+{
+    public Sprite[] Options;
+    protected virtual void Awake()
+    {
+        var random = Random.Range(0, 2);
+        Debug.Log(random);
+        GetComponent<SpriteRenderer>().sprite = Options[random];
+    }
+
+}
